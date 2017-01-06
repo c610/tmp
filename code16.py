06efbd001dbb8460a4e033f89ea4965697a79ebb
Cody Sixteen <610code@gmail.com>
2017-01-06 13:57:33 +0100

    Update code16.py

--- code16.py
+++ code16.py
@@ -25,15 +25,41 @@
 #
 
 # changelog:
+#   05.01.17 - removed super-cool-pass-protection
+#            - added colors
+#
 #   28.12.16 - added: reports to pdf ;P
 
+
+import thread
 import re
 import sys
 import subprocess
 import time
+
+# colors
+RED   = "\033[1;31m"
+BLUE  = "\033[1;34m"
+CYAN  = "\033[1;36m"
+GREEN = "\033[0;32m"
+REVERSE = "\033[;7m"
+ENDC = '\033[0m'
+YELLOW = '\033[93m'
+
+
+#
+def hello():
+  print '------------------------------------------------------------------------------'
+  print "                                 code16"
+  print '------------------------------------------------------------------------------'
+  print '  small wrapper for OpenVAS 6\n'
+
+
 ##
 # set target host/IP
 try:
+  hello()
+
   target = sys.argv[1]
 
   ##
@@ -51,7 +77,7 @@ try:
 
     if found:
       targetID = found.group(1)
-      print "[+] Found target ID:" + str(targetID)
+      print BLUE + "[+] Found target ID: " + ENDC + RED + str(targetID) + ENDC
 
   ##
   # prepare scan options (default full scan):
@@ -70,18 +96,18 @@ try:
 
     if found:
       taskID = found.group(1)
-      print '[+] Task ID = ', taskID
+      print GREEN + '[+] Task ID = ' + ENDC +  str(taskID)
 
 
   ##
   # run prepared taskID for targetID
-  print '[+] Running scan for ', target
+  print GREEN + '[+] Running scan for '+ ENDC +  RED + str(target) + ENDC
 
-  # yep, you will be asked for a pass here ;)
-  cmd = "omp -u admin --xml='<start_task task_id=\""+ taskID + "\"/>' > tmp.startID"
+  # yep, you will be asked for a pass here ;) # 05.01.17; not anymore
+  cmd = "omp -u admin -w letmein --xml='<start_task task_id=\""+ taskID + "\"/>' > tmp.startID"
   runme = subprocess.call([cmd], shell=True)
-  print '[+] Scan started... To get current status, see below:'# or type: omp -u admin -G'
-
+  print GREEN + '[+] Scan started... ' + ENDC + 'To get current status, see below:\n\t' + ENDC# or type: omp -u admin -G'
+  print YELLOW # 01
   # sleep few secs to get -G with our target:
   time.sleep(3)
 
@@ -91,18 +117,39 @@ try:
 
 
   while 'Done' not in open('tmp.stat','r').read():
-    print '[?]   ...zZzZZzZz...'
-    time.sleep(1)
+
+    # -- this part was found here: http://stackoverflow.com/a/3160917 ; big thanks!
+    def work():
+      time.sleep( 5 )
+
+    def locked_call( func, lock ):
+      lock.acquire()
+      func()
+      lock.release()
+
+    lock = thread.allocate_lock()
+    thread.start_new_thread( locked_call, ( work, lock, ) )
+
+    # This part is icky...
+    while( not lock.locked() ):
+      time.sleep( 0.1 )
+
+    while( lock.locked() ):
+      sys.stdout.write( "zZz" )
+      sys.stdout.flush()
+      time.sleep( 1 )
+    # --
+
     runme = subprocess.call([cmd2],shell=True)
 
-
-  print '[+] Scan looks to be done. Good.'
+  print ENDC # 02 - fin yellow
+  print GREEN + '\n\n[+] Scan looks to be done. Good.' + ENDC
 
   # target/taskID is scanned. rewriting results to report:
-  print '[+] Target scanned. Finished taskID : ' + str(taskID)
+  print GREEN + '[+] Target scanned. Finished taskID : ' + ENDC + RED + str(taskID) + ENDC
 
   # reports
-  print '[+] Cool! We can generate some reports now ... :)'
+  print CYAN + '[+] Cool! We can generate some reports now ... :)' + ENDC
 
   getXml = "omp -u admin -w letmein -X '<get_reports/> <report id><task id=\""+ str(taskID)  +"\"/>' > get.xml"
   #print getXml
@@ -122,23 +169,25 @@ try:
 
     if found:
       repID = found.group(1)
-      print '  [+] Found report ID : ' + repID
-      print '  [+] For taskID      : ' + taskID
+      print GREEN + '  [+] Found report ID : ' + repID + ENDC
+      print GREEN + '  [+] For taskID      : ' + taskID + ENDC
 
-      print ''
+      print '' + BLUE
       print '[+] Preparing report in PDF for %s ' % target
+      print ENDC
       repName = 'Report_for_'+str(target)+'.pdf'
 
       getRep = ("omp -u admin -w letmein --get-report %s --format c402cc3e-b531-11e1-9163-406186ea4fc5 > %s") % (repID, repName)
 
       runme = subprocess.call([getRep],shell=True)
 
-      print '[+] Report should be done in : ', repName
+      print '[+] Report should be done in : ' + GREEN +  str(repName) + ENDC
       # todo: check via sth like ls-la if rep.pdf is there
 
   print '[+] Thanks. Cheers!\n'
   #print '      Have fun ;)\n'
 
 except NameError, e:
-  print '[-] TargetID already exists, try different target host/IP'
+  print RED + '[-] TargetID already exists, try different target host/IP' + ENDC
+  print e
   pass
